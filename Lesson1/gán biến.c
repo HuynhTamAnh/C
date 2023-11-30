4101a2e677aa366efb667e7d066fcee6612f85f1
HuynhTamAnh <huynhtamanh2000@gmail.com>
2023-11-30 21:42:40 +0700

    change code

--- Lesson1/gán biến.c
+++ Lesson1/gán biến.c
@@ -2,12 +2,9 @@
 int main()
 {
     int a;
-    int b;
     printf("nhap a ");
     scanf("%d", &a);
     printf("nhap b: ");
-    scanf("%d", &b);
-    printf("a la: %d", b);
     printf("b la: %d", a);
     return 0;
 }
