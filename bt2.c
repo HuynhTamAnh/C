4e6d3ba836a12fed281e229d946c633bb1a9e00c
HuynhTamAnh <huynhtamanh2000@gmail.com>
2023-12-01 16:26:37 +0700

    change code

--- bt2.c
+++ bt2.c
@@ -2,18 +2,18 @@
 int main()
 {
     int a, b;
+    float n;
     printf("nhap a: ");
     scanf("%d", &a);
     printf("nhap b: ");
     scanf("%d", &b);
-    if (a==0) {
-        printf("ax+b=0 có 2 truong hop");
-    } if (b==0) {
-        printf("ax+b=0 có vo so nghiem");
-    } if (b!=0) {
+
+    if (b==0 && a==0){
+        printf("ax+b=0 co vo so nghiem");
+    } if (b!=0 && a==0){
         printf("ax+b=0 vo nghiem");
     } if (a!=0) {
-        printf("ax+b=0 co nghiem x= ", -b/a);
+        printf("ax+b=0 co nghiem x= %.2f ", n=-b/a);
     }
     return 0;
 }
