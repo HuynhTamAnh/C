8e73bff002e85387a72abfb1aafc6712add6856e
HuynhTamAnh <huynhtamanh2000@gmail.com>
2023-12-19 11:24:48 +0700

    feature/1912

--- PTIT_KS2023A_[HuynhTamAnh]/hackathon.c
+++ PTIT_KS2023A_[HuynhTamAnh]/hackathon.c
@@ -1,9 +1,25 @@
 #include<stdio.h>
 
 int m,n,choice;
+int sum;
 int a[100];
 
-void inp(){
+void menu(){
+    printf("************************MENU**************************\n");
+    printf("1. Nhap gia tri n phan tu cua mang (n nhap tu ban phim)\n");
+    printf("2. In gia tri cac phan tu trong mang\n");
+    printf("3. Tinh tong cac phan tu chia het cho 2 va 3 trong mang\n");
+    printf("4. In ra gia tri phan tu lon nhat va nho nhat trong mang\n ");
+    printf("5. Su dung thuat toan sap xep chen sap xep mang tang dan\n");
+    printf("6. Tinh tong cac phan tu la so nguyen to trong mang\n");
+    printf("7. Sap xep cac phan tu chan chia het cho 5 o đau mang theo thu tu giam dan, cac phan tu le chia het cho 5 ở cuoi mang theo thu tu tang dan,cac phan tu con lai o giua mang theo thu tu giam dan \n");
+    printf("8. Nhap gia tri m tu ban phim, chen gia tri m vao mang (sap xep tamg dan) dung vi tri \n");
+    printf("9. Thoat\n");
+}
+
+
+void inp()//case 1
+{
     printf("nhap so phan tu trong mang: ");
     scanf("%d",&n);
     for(int i=0;i<n;i++){
@@ -13,7 +29,8 @@ void inp(){
 
 }
 
-void outp(){
+void outp()//case2
+{
     printf("gia tri cac phan tu trong mang: \n");
     for(int i=0;i<n;i++){
         printf("%d\n",a[i]);
@@ -21,58 +38,55 @@ void outp(){
     printf("\n");
 }
 
-void average(){
+void chia23()//case3
+ {
     int sum=0;
-    int count=0;
         for(int i=0;i<n;i++){
-            if(a[i]>0){
+            if(a[i]%2==0&&a[i]%3==0){
                 sum+=a[i];
-                count++;
             }
         }
-        if (count>0){
-            float aver=(float)sum/count;
-            printf("Trung binh cac phan tu duong co trong mang: %.2f\n",aver);
-        }else printf("Khong co so duong\n");
+
 }
 
-void locate() {
-    int pos=-1;
-    int k,i;
-    printf("Gia tri phan tu muon tim: ");
-    scanf("%d", &k);
+void maxMin()//case4
+ {
+    int max,min;
+    max=a[0];
+    min=a[0];
+   for(int i=0;i<n;i++){
+       if (a[i]>a[0]) {
+           max = a[i];
 
-    for ( i = 0; i < n; i++) {
-        if (a[i] == k) {
-            pos = i+1;
-            break;
-        }
-    }
-    if (pos!=-1) {
-        printf("Phan tu co gia tri %d co vi tri %d\n", k, pos);
-    }else  printf("Phan tu co gia tri %d khong co trong mang\n",k);
+       }
+       if(a[i]<a[0]){
+           min =a[i];
+
+       }
+   }
+    printf("So lon nhat trong mang: %d\n", max);
+    printf("So nho nhat trong mang: %d\n",min);
 }
 
-void giamDan()
+void tangDan(int a[], int n)//case5
 {
-    int m,temp;
+    int m, j;
+    for (int i = 1; i < n; i++) {
+        m = a[i];
+        j = i - 1;
 
-    for(int i=0;i<m;i++){
-        for(int j=i+1;j<m;j++){
-            if(a[i]>a[j]){
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
+        while (j >= 0 && a[j] > m) {
+            a[j + 1] = a[j];
+            j = j - 1;
         }
-    }
-    for(int i=m-1;i>=0;i--){
-        printf("%d\n",a[i]);
+        a[j + 1] = m;
     }
 }
 
-int snt(int n){
+int snt(int n)//case 6
+{
     int uoc=0;
+    int sum=0;
     for(int i=1;i<=n;i++) {
         if (n % i == 0) {
             uoc++;
@@ -81,55 +95,37 @@ int snt(int n){
     return (uoc==2);
 }
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
-void customSort(int arr[], int n) {
-    int evenDiv3End = n - 1;
-    int oddDiv3Start = 0;
+void customSort(int a[], int n)//case7
+{
 
-    // Move odd numbers divisible by 3 to the beginning
-    for (int i = 0; i < n; i++) {
-        if (arr[i] % 2 != 0 && arr[i] % 3 == 0) {
-            swap(&arr[i], &arr[oddDiv3Start]);
-            oddDiv3Start++;
-        }
-    }
+    int i, j, temp;
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if ((a[i] % 5 == 0 && a[j] % 5 == 0 && a[i] < a[j]) || (a[i] % 5 != 0 && a[j] % 5 == 0)) {
 
-    // Move even numbers divisible by 3 to the end
-    for (int i = n - 1; i >= 0; i--) {
-        if (arr[i] % 2 == 0 && arr[i] % 3 == 0) {
-            swap(&arr[i], &arr[evenDiv3End]);
-            evenDiv3End--;
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
         }
     }
 }
 
-void insertIntoSortedArray(int arr[], int* n, int m) //case 8
+void chenSo(int m)//case 8
 {
     int i;
-    for (i = *n - 1; (i >= 0 && arr[i] < m); i--)
-        arr[i + 1] = arr[i];
-    arr[i + 1] = m;
-    (*n)++;
+    for( i=n-1;(i>=0&&a[i]>m);i--){
+    a[i+1]=a[i];
+}
+    a[i+1]=m;
+    n++;
 }
 
 int main() {
 
-    while (1) {
-        printf("************************MENU**************************\n");
-        printf("1. Nhap gia tri n phan tu cua mang (n nhap tu ban phim)\n");
-        printf("2. In gia tri cac phan tu trong mang\n");
-        printf("3. Tinh trung binh cac phan tu duong (>0) trong mang\n");
-        printf("4. In ra vi tri (chi so) cac phan tu co gia tri bang k trong mang (k nhap tu ban phim)\n ");
-        printf("5. Su dung thuat toan sap xep noi bot sap xep mang giam dan\n");
-        printf("6. Tinh so luong cac phan tu la so nguyen to trong mang\n");
-        printf("7. Sap xep cac phan tu chan chia het cho 3 o đau mang theo thu tu tang dan, cac phan tu le chia het cho 3 ở cuoi mang theo thu tu giam dan,cac phan tu con lai o giua mang theo thu tu tang dan \n");
-        printf("8. Nhap gia tri m tu ban phim, chen gia tri m vao mang (sap xep giam dan) đung vi tri \n");
-        printf("9. Thoat\n");
+   do{
+        menu();
         printf("choose: ");
         scanf("%d", &choice);
 
@@ -141,43 +137,47 @@ int main() {
                 outp();
                 break;
             case 3:
-                average();
+                chia23();
                 break;
             case 4:
-                locate();
+                maxMin();
                 break;
             case 5:
-                giamDan();
+                tangDan(a,n);
+                printf("Mang sau khi sap xep tang dan: ");
+                outp(a, n);
                 break;
             case 6:
+                sum = 0;
                 printf("cac so nguyen to trong mang la:\n ");
-                for(int i=0;i<n;i++) {
-                    if (snt(a[i]))
-                        printf("%d\n",a[i]);
+                for (int i = 0; i < n; i++) {
+                    if (snt(a[i])) {
+                        sum += a[i];
+                        printf("%d\n", a[i]);
+                    }
                 }
+                printf("Tong cac so nguyen to trong mang: %d\n", sum);
                 break;
             case 7:
-                // Sort the array
                 customSort(a, n);
-
-                // Print the sorted array
-                printf("Mang sau khi sap xep:\n");
-                for (int i = 0; i < n; i++) {
-                    printf("%d\n", a[i]);
-                }
+                outp(a, n);
+                break;
             case 8:
                 printf("Nhap gia tri m: ");
                 scanf("%d", &m);
-                insertIntoSortedArray(a, &n, m);
+
+                chenSo(m);
                 printf("Mang sau khi chen: ");
                 for (int i = 0; i < n; i++) {
                     printf("%d\n", a[i]);
                 }
                 break;
             case 9:
-                break;
-            default: printf("nhap sai lenh");
+                return 0;
+            default: printf("nhap sai lenh roi nhe\n");
 
         }
-    }
+    }while (choice!=9);
+   return 0;
 }
+
