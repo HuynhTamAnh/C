19725ac4dd5effdfdbad4ab6dd3ecc2561c9bc5d
HuynhTamAnh <huynhtamanh2000@gmail.com>
2024-01-03 08:41:09 +0700

    feature/0301

--- doc file/bt.c
+++ doc file/bt.c
@@ -20,42 +20,42 @@
     }
 
     int main() {
-        FILE *inputFile, *outputFile;
+        FILE *inp, *outp;
         char line[100];
         int soHs;
 
-        inputFile = fopen("sinhvien.txt", "r");
-        if (inputFile == NULL) {
+        inp = fopen("sinhvien.txt", "r");
+        if (inp == NULL) {
             perror("Không thể mở file sinhvien.txt.txt");
             return 1;
         }
 
 
-        fscanf(inputFile, "%d", &soHs);
-        fgetc(inputFile); // Đọc và loại bỏ ký tự xuống dòng
+        fscanf(inp, "%d", &soHs);
+        fgetc(inp);
 
-        outputFile = fopen("sinhvien_out.txt", "w");
-        if (outputFile == NULL) {
+        outp = fopen("sinhvien_out.txt", "w");
+        if (outp == NULL) {
             perror("Không thể tạo hoặc mở file sinhvien_out.txt");
-            fclose(inputFile);
+            fclose(inp);
             return 1;
         }
 
         for (int i = 0; i < soHs; i++) {
-            if (fgets(line, sizeof(line), inputFile) != NULL) {
+            if (fgets(line, sizeof(line), inp) != NULL) {
 
                 char *token = strtok(line, "\n");
                 lucDau(token);
 
 
                 printf("%s\n", token);
-                fprintf(outputFile, "%s\n", token);
+                fprintf(outp, "%s\n", token);
             }
         }
 
 
-        fclose(inputFile);
-        fclose(outputFile);
+        fclose(inp);
+        fclose(outp);
 
         return 0;
     }
